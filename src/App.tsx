6bff121058276d77e0c94d9d9fa258b8b132ca30
Dhruvi <dhruvihl369@gmail.com>
2024-07-17 20:28:31 -0400

    added modal and datacontainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,79 @@
-import { Button, Container, Logo } from "./styled";
+import {
+  Button,
+  Close,
+  Container,
+  DataContainer,
+  InputContainer,
+  InputFiled,
+  Label,
+  Logo,
+  Modal,
+  ModalContent,
+} from "./styled";
 import logo from "./logo.png";
 import { useCallback, useState } from "react";
 
 const App = () => {
-  const [showPlaySelection, setShowPlaySection] = useState<boolean>(false);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [showPlaySection, setShowPlaySection] = useState<boolean>(false);
+  const [showDataSection, setShowDataSection] = useState<boolean>(false);
+  const [numberOfPlayers, setNumberOfPlayers] = useState<number>(0);
+  const [feedbackMessage, setFeedBackMessage] = useState<string>("");
 
   const handlePlayButton = useCallback(() => {
     setShowPlaySection(true);
+    setIsPlaying(true);
   }, []);
+
+  const handleStartGame = useCallback(() => {
+    if (numberOfPlayers <= 0) {
+      setFeedBackMessage("Please enter a vaild number of players");
+      return;
+    }
+    setShowPlaySection(false);
+    setShowDataSection(true);
+  }, [numberOfPlayers]);
+
   return (
     <Container>
       <Logo src={logo} />
       <h2>Guess the Number of Jelly Beans (1 to 100)</h2>
-      {!showPlaySelection && (
-        <Button onClick={() => handlePlayButton()}>Play</Button>
+      {!showPlaySection && (
+        <Button disabled={isPlaying} onClick={() => handlePlayButton()}>
+          Play
+        </Button>
       )}
+      {showPlaySection && (
+        <InputContainer>
+          <Label>Number of Players: </Label>
+          <InputFiled
+            type="number"
+            min="1"
+            value={numberOfPlayers}
+            onChange={(event) => setNumberOfPlayers(event.target.valueAsNumber)}
+          />
+          <Button onClick={() => handleStartGame()}>Start</Button>
+        </InputContainer>
+      )}
+      {showDataSection && (
+        <DataContainer>
+          <InputContainer>
+            <Label>Name: </Label>
+            <InputFiled type="text" />
+          </InputContainer>
+          <InputContainer>
+            <Label>Guess: </Label>
+            <InputFiled type="number" min="1" max="100" />
+          </InputContainer>
+          <Button>Submit</Button>
+        </DataContainer>
+      )}
+      <Modal show={feedbackMessage !== ""}>
+        <ModalContent>
+          {feedbackMessage}
+          <Close onClick={() => setFeedBackMessage("")}>&times;</Close>
+        </ModalContent>
+      </Modal>
     </Container>
   );
 };
--- src/styled.ts
+++ src/styled.ts
@@ -1,12 +1,19 @@
 import styled from "styled-components";
+import { ModalProps } from "./types";
 
 const Container = styled.div`
   text-align: center;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
   padding: 20px;
 `;
 
 const Logo = styled.img`
   width: 200px;
+  @media (max-width: 300px) {
+    width: 100px;
+  }
 `;
 
 const Button = styled.button`
@@ -18,6 +25,92 @@ const Button = styled.button`
   color: #fff;
   border-radius: 10px;
   cursor: pointer;
+  &:disabled {
+    background-color: gray;
+    cursor: not-allowed;
+  }
 `;
 
-export { Container, Logo, Button };
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  padding-top: 20px;
+`;
+
+const Label = styled.label`
+  font-weight: 600;
+`;
+
+const InputFiled = styled.input`
+  font-size: 16px;
+  width: 200px;
+  @media (max-width: 300px) {
+    width: 100px;
+  }
+`;
+
+const Modal = styled.div<ModalProps>`
+  display: ${({ show }) => (show ? "block" : "none")};
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100vh;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  overflow: auto;
+`;
+
+const ModalContent = styled.div`
+  background-color: #fff;
+  margin: 30% auto;
+  padding: 30px;
+  border: 1px solid #888;
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  border-radius: 10px;
+`;
+
+const Close = styled.span`
+  color: #aaa;
+  font-size: 28px;
+  font-weight: 600;
+  cursor: pointer;
+
+  &:hover,
+  &:focus {
+    color: black;
+    text-decoration: none;
+  }
+`;
+
+const DataContainer = styled.div`
+  margin: 30px;
+  padding: 20px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  width: 50%;
+  border-radius: 10px;
+  background: #fff;
+`;
+
+export {
+  Container,
+  Logo,
+  Button,
+  InputContainer,
+  Label,
+  InputFiled,
+  Modal,
+  ModalContent,
+  Close,
+  DataContainer,
+};
